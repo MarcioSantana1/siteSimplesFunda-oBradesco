17feecde29a6211cbb546ecb6e84111343a17e07
Marcio Santana <marciosantana777@gmail.com>
2022-09-04 16:48:53 -0300

    first-commit

---
+++ bradesco.js
@@ -0,0 +1,20 @@
+'usestrict'
+const switcher = document.querySelector('.btn');
+switcher.addEventListener ('click' , function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+if(className == "ligth-theme") {
+    this.textContent = "Dark";
+}
+else {
+    this.textContent = "ligth";
+}
+console.log('current class name: ' + className);
+
+});
+
+
+
+
+
